f8c8f5e31e95e1b908836a8bf384cf1f6c2c6cf0
DaeKyoung <jdk7167@naver.com>
2016-09-28 15:17:52 +0900

    ㅎㅇ?

--- aromainlife/src/main/java/com/knowledge_seek/aromainlife/HomeController.java
+++ aromainlife/src/main/java/com/knowledge_seek/aromainlife/HomeController.java
@@ -31,7 +31,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate +"branch test" );
+		model.addAttribute("serverTime", formattedDate +"branch test2" );
 		
 		return "home";
 	}
