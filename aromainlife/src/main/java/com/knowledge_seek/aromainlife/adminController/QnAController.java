cb5bc1e4fdb15c2d1be88c6af783f4375d844541
DaeKyoung <jdk7167@naver.com>
2016-09-29 20:35:06 +0900

    문의사항 글쓰기 구현중..

--- aromainlife/src/main/java/com/knowledge_seek/aromainlife/adminController/QnAController.java
+++ aromainlife/src/main/java/com/knowledge_seek/aromainlife/adminController/QnAController.java
@@ -1,27 +1,99 @@
 package com.knowledge_seek.aromainlife.adminController;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.knowledge_seek.aromainlife.domain.QnA;
+import com.knowledge_seek.aromainlife.domain.QnA;
+import com.knowledge_seek.aromainlife.service.QnAService;
+import com.knowledge_seek.aromainlife.service.impl.QnAServiceImpl;
+import com.knowledge_seek.aromainlife.util.PagingUtil;
 
 @Controller
 @RequestMapping("/qna")
 public class QnAController {
 
-	@RequestMapping(value = "/list.do")
-	public String prolist( Model model) {
+	@Value("${PAGESIZE}")
+	private int pageSize; 
+	@Value("${BLOCKPAGE}")
+	private int blockPage;
+	
+	@Resource(name="qnaService")
+	QnAServiceImpl qnaService;
+	
+	@RequestMapping("/list.do")
+	public String list(@RequestParam(defaultValue="1",required=false,value="nowPage") int nowPage,
+			@RequestParam Map map,Model model,HttpServletRequest req) {
+		
+		int totalRecordCount =qnaService.getTotalRecordCount(map);
+		int totalPage= (int)(Math.ceil(((double)totalRecordCount/pageSize)));
+		
+		//시작 및 끝 ROWNUM구하기]
+		int start= (nowPage-1)*pageSize+1;
+		int end = nowPage*pageSize;		
+		map.put("start", start);
+		map.put("end",end);
+		
+		List<QnA> lists=qnaService.selectList(map);
+		String pagingString = PagingUtil.pagingText(totalRecordCount, pageSize, blockPage, nowPage, req.getContextPath()+"/qna/list.do?");
+		
+		model.addAttribute("lists",lists);
+		model.addAttribute("pagingString",pagingString);
+		model.addAttribute("totalPage",totalPage);
+		model.addAttribute("nowPage",nowPage);
+		model.addAttribute("totalRecordCount",totalRecordCount);
+		model.addAttribute("pageSize",pageSize);
+		
+		
 		
 		return "/admin/qna";
 	}
-	@RequestMapping(value = "/write.do")
-	public String write( Model model) {
+
+	@RequestMapping("/writeForm.do")
+	public String writeForm( Model model) {
 		
 		return "/admin/qnaWrite";
 	}
-	@RequestMapping(value = "/edit.do")
+	
+	@RequestMapping(value = "/write.do")
+	public String write( Model model) {
+		
+		return "forward:/qna/list.do";
+	}
+		
+	@RequestMapping("/editForm.do")
+	public String updateForm(QnA qna,Model model){
+		qna=qnaService.selectOne(qna);
+		model.addAttribute("QnA",qna);
+
+		return "/admin/qnaEdit";
+	}
+	
+	@RequestMapping("/edit.do")
 	public String edit( Model model) {
 		
-		return "/admin/qnaEdit";
+		return "/admin/qnaView";
+	}
+	@RequestMapping("/delete.do")
+	public String delete(QnA QnA,@RequestParam(defaultValue="1",required=false,value="nowPage") int nowPage){
+		//QnA=qnaService.selectOne(QnA);
+		qnaService.delete(QnA);
+		/*if(QnA.getFile_id()!=null){
+			//파일 삭제 
+			FileDTO FileDto =fileServiceImpl.selectFileDetail(QnA.getFile_id());
+			
+		}*/
+		
+		return "redirect:/pro/list.do?nowPage="+nowPage;
 	}
 	
 }
--- aromainlife/src/main/java/com/knowledge_seek/aromainlife/domain/QnA.java
+++ aromainlife/src/main/java/com/knowledge_seek/aromainlife/domain/QnA.java
@@ -1,5 +1,82 @@
 package com.knowledge_seek.aromainlife.domain;
 
+import java.sql.Date;
+
 public class QnA {
+	
+	
+	private String qnaNo;
+	private String title;
+	private String writer;
+	private String	tel;
+	private Date createDate;
+	private String content;
+	private int hits;
+	private String pass;
+	private String file_id;
+	private int answerChk;
+	public String getQnaNo() {
+		return qnaNo;
+	}
+	public void setQnaNo(String qnaNo) {
+		this.qnaNo = qnaNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getFile_id() {
+		return file_id;
+	}
+	public void setFile_id(String file_id) {
+		this.file_id = file_id;
+	}
+	public int getAnswerChk() {
+		return answerChk;
+	}
+	public void setAnswerChk(int answerChk) {
+		this.answerChk = answerChk;
+	}  
+	 
+	
+	
 
 }
